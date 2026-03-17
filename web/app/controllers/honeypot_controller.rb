e09d52b5c756281724394b28c20489fbdf1848e0
Ross Ethridge <ross.ethridge@gmail.com>
2026-03-16 19:53:56 -0500

    Adding redirect

--- web/app/controllers/honeypot_controller.rb
+++ web/app/controllers/honeypot_controller.rb
@@ -2,6 +2,6 @@ class HoneypotController < ApplicationController
   skip_before_action :require_authentication, raise: false
 
   def trap
-    redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ", allow_other_host: true
+    redirect_to "https://youtu.be/yvqVg7jsQC4?si=m_y9vPUQljF3XbMz", allow_other_host: true
   end
 end
